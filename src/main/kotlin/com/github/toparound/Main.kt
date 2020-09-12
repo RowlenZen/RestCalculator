95a7817f3083f7a33f435c105defd720d88fa1ae
Anton <makarovtonyy@gmail.com>
2020-09-12 20:40:25 +0200

    added calculate method

---
+++ src/main/kotlin/com/github/toparound/Main.kt
@@ -0,0 +1,12 @@
+package com.github.toparound
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class Main {
+
+}
+fun main() {
+    SpringApplication.run(Main::class.java)
+}
